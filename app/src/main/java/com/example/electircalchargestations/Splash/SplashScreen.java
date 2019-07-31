061c880a7fd0fcad8b092ffc868bbee821752f32
Batur Tulek <baturtulek97@gmail.com>
2019-07-31 15:43:57 +0300

    Added horizontal progress bar to splash screen

--- app/src/main/java/com/example/electircalchargestations/Splash/SplashScreen.java
+++ app/src/main/java/com/example/electircalchargestations/Splash/SplashScreen.java
@@ -5,6 +5,8 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.os.Handler;
 import com.example.electircalchargestations.CustomDialog;
 import com.example.electircalchargestations.Main.MainActivity;
 import com.example.electircalchargestations.R;
@@ -12,26 +14,62 @@ import com.example.electircalchargestations.Utilities;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private final int THREAD_SLEEP_TIME = 3000;
+    private ProgressBar  progressBar;
+    private int          progressStatus                 = 0;
+    private final int    PROGRESS_BAR_MAX               = 100;
+    private final int    PROGRESS_BAR_INCREMENT         = 1;
+    private final int    THREAD_SLEEP_SPLASH_SCREEN     = 3000;
+    private final int    THREAD_SLEEP_PROGRESSBAR       = 20;
+    private final String ERROR_HEADING                  = "Connection Error";
+    private final String ERROR_MESSAGE                  = "You are not connected to Internet.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_splash_screen);
+        progressBar = findViewById(R.id.spalshprogressBar);
 
         if(Utilities.checkInternetConnection(SplashScreen.this)){
             displaySplashScreen();
+            displayProgressBar();
         }else{
-            showAlertDialog(SplashScreen.this);
+            displayAlertDialog(SplashScreen.this);
         }
+    }
+    
+    private void displayProgressBar(){
+        Handler handler     = new Handler();
+
+        if (progressStatus == PROGRESS_BAR_MAX) {
+            progressStatus = 0;
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (progressStatus < PROGRESS_BAR_MAX) {
+                    progressStatus += PROGRESS_BAR_INCREMENT;
+                    try {
+                        Thread.sleep(THREAD_SLEEP_PROGRESSBAR);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                        }
+                    });
+                }
+            }
+        }).start();
     }
 
     private void displaySplashScreen() {
         Thread timerTread = new Thread() {
             public void run() {
                 try {
-                    sleep(THREAD_SLEEP_TIME);
+                    sleep(THREAD_SLEEP_SPLASH_SCREEN);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
@@ -48,8 +86,8 @@ public class SplashScreen extends AppCompatActivity {
         finish();
     }
 
-    private void showAlertDialog(Activity activity) {
-        CustomDialog dialog = new CustomDialog(activity,"Connection Error", "You are not connected to Internet.");
+    private void displayAlertDialog(Activity activity) {
+        CustomDialog dialog = new CustomDialog(activity,ERROR_HEADING, ERROR_MESSAGE);
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         dialog.setCancelable(false);
         dialog.show();
